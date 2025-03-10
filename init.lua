94bf540a8332026ee3460153848a46972ca90858
jappy <108874513+averagejv@users.noreply.github.com>
2025-03-10 23:39:19 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 --
 -- very cool
 
+vim.g.python_indent = {}
 -- show function when K is pressed
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover documentation" })
 print("welcome back <3")
